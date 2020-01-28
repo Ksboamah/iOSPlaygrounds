98ec0d39ca6e8df574d530c58f3a061b4954e180
Kwame <bkwamesakyi@gmail.com>
2020-01-28 08:56:59 -0500

    Finished PlaygroundBasic

--- PlaygroundBasics.playground/Pages/Calculations.xcplaygroundpage/Contents.swift
+++ PlaygroundBasics.playground/Pages/Calculations.xcplaygroundpage/Contents.swift
@@ -3,14 +3,17 @@
 //:
 //: You can use the plus `+` and minus `-` signs for addition and subtraction, just as you’d expect:
 34 + 56 + 230
-
+65 + 21 + 95
 1000 - 300
-
+50093 - 576
 //: For multiplication, use the asterisk `*` :
 3 * 100
+5 * 36
+
 //: For division, use the slash `/` :
 24 / 8
 22 / 7
+22 / 11
 
 //: For grouping, use parenthesis `( )` :
 (300 + 500 + 400) / 3
--- PlaygroundBasics.playground/Pages/What Is a Playground?.xcplaygroundpage/Contents.swift
+++ PlaygroundBasics.playground/Pages/What Is a Playground?.xcplaygroundpage/Contents.swift
@@ -2,14 +2,15 @@
 //: A playground is a place where you can play and experiment with code — and see instant results — without anything getting in your way. A playground mixes parts that explain things (like this text) with live code that you can change.
 //:
 //: The simplest _line of code_ you can write is just a number:
-76000
-
+7000
+6346
+-4566
 
 //:👆Notice how the number above looks different?
 //: This style of text is *editable code*, which means you can write it or change it to make stuff happen.
 //:
 //: For negative numbers use the minus sign:
--1200
+-120
 /*:
 Notice how the numbers also show up in the gray area to the right? 👉
 
