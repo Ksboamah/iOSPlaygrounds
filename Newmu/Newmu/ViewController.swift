fe871d6b404aa7aecc9abe84459b5b09b5f82d70
kwameboamah <bwamesakyi@gmail.com>
2020-04-14 19:08:17 -0400

    I have only created the file, but have'nt began woring on it because I am currently swamped with work. I will pick up with full force tomorrow.

---
+++ Newmu/Newmu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Newmu
+//
+//  Created by Kwame  Boamah  on 4/14/20.
+//  Copyright © 2020 Kwame  Boamah . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
