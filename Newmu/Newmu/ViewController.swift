2a20e45d1b2897fdb10c24d81c48c5fed1ecdbfb
kwameboamah <bwamesakyi@gmail.com>
2020-04-16 22:45:44 -0400

    I created a basic layout for the first scrren.

--- Newmu/Newmu/ViewController.swift
+++ Newmu/Newmu/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  Newmu
 //
-//  Created by Kwame  Boamah  on 4/14/20.
+//  Created by Kwame  Boamah  on 4/16/20.
 //  Copyright © 2020 Kwame  Boamah . All rights reserved.
 //
 
@@ -12,9 +12,19 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
