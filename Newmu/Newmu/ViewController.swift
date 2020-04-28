68498426aeba6091319254f29879a921c26ce1f3
kwameboamah <bwamesakyi@gmail.com>
2020-04-28 18:35:04 -0400

    I added the extention of my view controller that would allow me to find the text I need.

--- Newmu/Newmu/ViewController.swift
+++ Newmu/Newmu/ViewController.swift
@@ -8,20 +8,40 @@
 
 import UIKit
 
+extension UIViewController {
+    func regexMatches(pattern: String) -> Array<String> {
+        let re: NSRegularExpression
+        do {
+            re = try NSRegularExpression(pattern: pattern, options: [])
+        } catch {
+            return []
+        }
+
+        let matches = re.matches(in: self, options: [], range: NSRange(location: 0, length: self.utf16.count))
+        var songList: Array<String> = []
+        for match in matches {
+            // range at index 0: full match
+            // range at index 1: first capture group
+            let substring = (self as NSString).substring(with: match.range(at: 1))
+            songList.append(substring)
+        }
+        return songList
+    }
+    
+}
+
+
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var songList: [String] = []
         
         if let url = URL(string: "https://www.rollingstone.com/charts/songs/") {
             do {
                 let contents = try String(contentsOf: url)
-                let input = contents
-                let regex = try NSRegularExpression(pattern: "c-chart__table--title\">\(String())<p><" , options: NSRegularExpression.Options.caseInsensitive)
-                let matches = regex.matches(in: input, options: [], range: NSRange(location: 0, length: input.utf16.count))
-                print(matches)
+                print(contents)
                 
             } catch {
                 // contents could not be loaded
@@ -32,7 +52,9 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    override func regexMatches(pattern: "title\"><p>Life is Good</p>") -> Array<String> {
+        <#code#>
+    }
     /*
     // MARK: - Navigation
 
@@ -44,3 +66,7 @@ class ViewController: UIViewController {
     */
 
 }
+
+
+
+
